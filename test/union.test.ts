d9bdb4d391d96209e18dc70583f8a5bc97883686
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-08-03 17:30:40 +0700

    union type

---
+++ test/union.test.ts
@@ -0,0 +1,27 @@
+describe("Union type", () => {
+	it("should support in typescript", function () {
+		let sample: number | string | boolean = "sandy";
+		console.info(sample);
+
+		sample = 100;
+		console.info(sample);
+
+		sample = true;
+		console.info(sample);
+	});
+	it("should support typeof operator", function () {
+		function process(value: number | string | boolean) {
+			if (typeof value === "string") {
+				return value.toUpperCase();
+			} else if (typeof value === "number") {
+				return value + 2;
+			} else {
+				return !value;
+			}
+		}
+
+		expect(process("sandy")).toBe("SANDY");
+		expect(process(100)).toBe(102);
+		expect(process(true)).toBe(false);
+	});
+});
