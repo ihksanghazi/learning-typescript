679c63bd880f21fda1bd1955044dfb7dfeada4e4
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-08-03 14:55:59 +0700

    tipe data primitif

---
+++ test/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe("data type", () => {
+	it("should must be declare", function () {
+		const name: string = "Nursandy Ihksan";
+		const balance: number = 100;
+		const isVIP: boolean = true;
+
+		console.info(name);
+		console.info(balance);
+		console.info(isVIP);
+	});
+});
