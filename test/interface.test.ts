126fbe4df7bc6487ff1e0981ff1f9fe959db907f
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-08-04 10:05:55 +0700

    intersection types

--- test/interface.test.ts
+++ test/interface.test.ts
@@ -86,4 +86,22 @@ describe("Interface", () => {
 
 		console.info(person.sayHello("Budi"));
 	});
+
+	it("should support intersection types", function () {
+		interface HasName {
+			name: string;
+		}
+		interface HasId {
+			id: string;
+		}
+
+		type Domain = HasId & HasName;
+
+		const domain: Domain = {
+			id: "1",
+			name: "Sandy",
+		};
+
+		console.info(domain);
+	});
 });
