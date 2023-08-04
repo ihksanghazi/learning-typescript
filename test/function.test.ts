828bf5be282a9c7bc3b14a197d1c8182d240e931
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-08-04 12:38:52 +0700

    function parameter

--- test/function.test.ts
+++ test/function.test.ts
@@ -12,4 +12,38 @@ describe("Function", () => {
 
 		printHello("Sandy");
 	});
+
+	it("should support default value", function () {
+		function sayHello(name: string = "Guest"): string {
+			return `Hello ${name}`;
+		}
+
+		expect(sayHello("Sandy")).toBe("Hello Sandy");
+		expect(sayHello()).toBe("Hello Guest");
+	});
+
+	it("should support rest parameter", function () {
+		function sum(...values: number[]): number {
+			let total = 0;
+			for (const value of values) {
+				total += value;
+			}
+			return total;
+		}
+
+		expect(sum(1, 2, 3, 4, 5)).toBe(15);
+	});
+
+	it("should support optional parameter", function () {
+		function sayHello(firstname: string, lastname?: string): string {
+			if (lastname) {
+				return `Hello ${firstname} ${lastname}`;
+			} else {
+				return `Hello ${firstname}`;
+			}
+		}
+
+		expect(sayHello("Nur", "Sandy")).toBe("Hello Nur Sandy");
+		expect(sayHello("Nur")).toBe("Hello Nur");
+	});
 });
