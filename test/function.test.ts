58aecc596a3ac951776a5b46ea4dd8b53f53b98c
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-08-04 12:47:25 +0700

    function overloading

--- test/function.test.ts
+++ test/function.test.ts
@@ -46,4 +46,19 @@ describe("Function", () => {
 		expect(sayHello("Nur", "Sandy")).toBe("Hello Nur Sandy");
 		expect(sayHello("Nur")).toBe("Hello Nur");
 	});
+
+	it("should support function overloading", function () {
+		function callMe(value: number): number;
+		function callMe(value: string): string;
+		function callMe(value: any): any {
+			if (typeof value === "string") {
+				return value.toUpperCase();
+			} else if (typeof value === "number") {
+				return value * 10;
+			}
+		}
+
+		expect(callMe(10)).toBe(100);
+		expect(callMe("sandy")).toBe("SANDY");
+	});
 });
