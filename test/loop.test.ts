88fc6f23791c71acefd523d426306191901705ec
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-08-05 11:08:54 +0700

    for loop

---
+++ test/loop.test.ts
@@ -0,0 +1,17 @@
+describe("Loop", () => {
+	it("should support for loop", function () {
+		const names: string[] = ["Nur", "Sandy", "Ihksan"];
+
+		for (let i = 0; i < names.length; i++) {
+			console.info(names[i]);
+		}
+
+		for (const name of names) {
+			console.info(name);
+		}
+
+		for (const index in names) {
+			console.info(names[index]);
+		}
+	});
+});
