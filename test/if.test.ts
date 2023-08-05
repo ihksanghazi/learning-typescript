52bb09ba807d0c5b80862aa20074cbdc9c1cf88e
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-08-05 10:53:41 +0700

    if statement

---
+++ test/if.test.ts
@@ -0,0 +1,12 @@
+describe("If Statement", () => {
+	it("should support in typescript", function () {
+		const examValue = 90;
+		if (examValue > 80) {
+			console.info("Good");
+		} else if (examValue > 60) {
+			console.info("Not Bad");
+		} else {
+			console.info("Try Again");
+		}
+	});
+});
